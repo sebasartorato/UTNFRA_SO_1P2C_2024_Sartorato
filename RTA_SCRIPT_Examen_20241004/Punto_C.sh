bfbb8a531d69f738a68ed0a87bf25fe8610f4cd4
sebasartorato <sebastiansartorato@hotmail.com>
2024-10-09 04:02:04 +0000

    Punto C

---
+++ RTA_SCRIPT_Examen_20241004/Punto_C.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SHELL="/bin/bash"
+LISTA=$1
+USER_HASH=$(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}')
+
+ANT_IFS=$IFS
+IFS=$'\n'
+
+#Creo grupos primero
+sudo groupadd p1c2_2024_gAlumno
+sudo groupadd p1c2_2024_gProfesores
+
+#Creo usuarios con sus grupos
+for LINEA in `cat $LISTA | grep -v ^#`
+do
+        USUARIO=$(echo $LINEA | awk -F ':' '{print $1}')
+        GRUPO=$(echo $LINEA | awk -F ':' '{print $2}')
+        echo "Creando usuario: $USUARIO en el grupo: $GRUPO"
+        
+        sudo useradd -m -s $SHELL -p $USER_HASH -G $GRUPO $USUARIO
+done
+
+IFS=$ANT_IFS
+
+sudo chown p1c2_2024_A1:p1c2_2024_A1 /Examenes-UTN/alumno_1
+sudo chown p1c2_2024_A2:p1c2_2024_A2 /Examenes-UTN/alumno_2
+sudo chown p1c2_2024_A3:p1c2_2024_A3 /Examenes-UTN/alumno_3
+sudo chown p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
+
+sudo chmod 750 /Examenes-UTN/alumno_1
+sudo chmod 760 /Examenes-UTN/alumno_2
+sudo chmod 700 /Examenes-UTN/alumno_3
+sudo chmod 775 /Examenes-UTN/profesores
+
+#Creo archivo validar.txt
+
+sudo su -c "whoami > /Examenes-UTN/alumno_1/validar1.txt" p1c2_2024_A1
+sudo su -c "whoami > /Examenes-UTN/alumno_2/validar2.txt" p1c2_2024_A2
+sudo su -c "whoami > /Examenes-UTN/alumno_3/validar3.txt" p1c2_2024_A3
+sudo su -c "whoami > /Examenes-UTN/profesores/validar4.txt" p1c2_2024_P1
+
