d8621283bfe58657eaf71f183bc256c80618fdaa
sebasartorato <sebastiansartorato@hotmail.com>
2024-10-10 22:50:22 +0000

    Change

--- RTA_SCRIPT_Examen_20241004/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_D.sh
@@ -7,14 +7,14 @@ sudo mkdir -p $HOME/Estructura_Asimetrica/{correo,cliente}
 #Creo archivos de cartas
 for i in {1..100}
 do
-        sudo touch $HOME/Estructura_Asimetrica/correo/cartas_$i
-        sudo touch $HOME/Estructura_Asimetrica/cliente/cartas_$i
+        sudo mkdir -p $HOME/Estructura_Asimetrica/correo/cartas_$i
+        sudo mkdir -p $HOME/Estructura_Asimetrica/cliente/cartas_$i
 done
 
 #Creo archivos de carteros para el dir correo
 for i in {1..10}
 do
-        sudo touch $HOME/Estructura_Asimetrica/correo/carteros_$i
+        sudo mkdir -p $HOME/Estructura_Asimetrica/correo/carteros_$i
 done
 
 #Muestro resultado
